174c88b18a8309de749de62055f2d3d0c6206842
JarneDemoen <jarne.demoen@vub.be>
2025-09-27 10:25:13 +0200

    Integrated face detection

--- emotion_model.pl
+++ emotion_model.pl
@@ -1,3 +1,8 @@
-nn(emotion_net,[Image],Emotion,['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']) :: face(Image, Emotion).
-
-happy(Image) :- face(Image, 'happy').
+nn(emotion_net, [X], Y, [angry, disgust, fear, happy, neutral, sad, surprise]) :: emotion(X, Y).
+positive_emotion(X) :- emotion(X, happy).
+positive_emotion(X) :- emotion(X, surprise).
+negative_emotion(X) :- emotion(X, angry).
+negative_emotion(X) :- emotion(X, sad).
+negative_emotion(X) :- emotion(X, disgust).
+negative_emotion(X) :- emotion(X, fear).
+neutral_emotion(X) :- emotion(X, neutral).
