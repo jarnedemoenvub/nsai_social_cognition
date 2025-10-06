5b853afd7280f35d4f2b0255e4f936bb28764ed5
JarneDemoen <jarne.demoen@vub.be>
2025-10-06 15:43:01 +0200

    Stuck on how to come to a final query answer that can be used for training

--- code/model.pl
+++ code/model.pl
@@ -17,11 +17,177 @@ face_emotion_prob(Image, FaceIdx, EmotionIdx) :-
 faces(FaceImg, AllEmotions) :-
     all_face_emotions(FaceImg, AllEmotions).
 
+dominant_face_emotion(Image, FaceIdx, EmotionIdx) :-
+    between(0, 4, FaceIdx),
+    face_emotion_prob(Image, FaceIdx, EmotionIdx),
+    \+ (face_emotion_prob(Image, FaceIdx, OtherEmotionIdx),
+        OtherEmotionIdx \= EmotionIdx).
+
 % Scene prediction 
 scene(SceneImg, SceneIdx) :-
     scene_prediction(SceneImg, SceneIdx).
 
-% Mapping of model emotions to dataset emotions
-mapped_emotion(0, anger).
-mapped_emotion(0, rage).
+% Learn mapping of model emotions to dataset emotions
+% Angry -> rage, annoyance
+t(_)::mapped_emotion(0, 4).
+t(_)::mapped_emotion(0, 13).
+t(_)::mapped_emotion(0, 14).
+
+% Disgust -> disgust, loathing
+t(_)::mapped_emotion(1, 23).
+t(_)::mapped_emotion(1, 21).
+
+% Fear -> fear, apprehension, terror, vigilance
+t(_)::mapped_emotion(2, 10).
+t(_)::mapped_emotion(2, 2).
+t(_)::mapped_emotion(2, 16).
+t(_)::mapped_emotion(2, 11).
+
+% Happy -> joy, ecstasy, serenity, admiration, acceptance
+t(_)::mapped_emotion(3, 5).
+t(_)::mapped_emotion(3, 9).
+t(_)::mapped_emotion(3, 7).
+t(_)::mapped_emotion(3, 20).
+t(_)::mapped_emotion(3, 15).
+
+% Sad -> sadness, grief, pensiveness
+t(_)::mapped_emotion(4, 12).
+t(_)::mapped_emotion(4, 6).
+t(_)::mapped_emotion(4, 19).
+
+% Surprise -> surprise, amazement, distraction
+t(_)::mapped_emotion(5, 18).
+t(_)::mapped_emotion(5, 17).
+t(_)::mapped_emotion(5, 22).
+
+% Neutral -> boredom, serenity, acceptance
+t(_)::mapped_emotion(6, 8).
+t(_)::mapped_emotion(6, 7).
+t(_)::mapped_emotion(6, 15).
+% Places that trigger happiness
+t(_)::joy_place(7).      % amusement_park
+t(_)::joy_place(11).     % arcade
+t(_)::joy_place(34).     % ball_pit
+t(_)::joy_place(35).     % ballroom
+t(_)::joy_place(42).     % baseball_field
+t(_)::joy_place(48).     % beach
+t(_)::joy_place(49).     % beach_house
+t(_)::joy_place(62).     % botanical_garden
+t(_)::joy_place(77).     % campus
+t(_)::joy_place(80).     % candy_store
+t(_)::joy_place(83).     % carrousel
+t(_)::joy_place(97).     % coast
+t(_)::joy_place(119).    % diner/outdoor
+t(_)::joy_place(120).    % dining_hall
+t(_)::joy_place(121).    % dining_room
+t(_)::joy_place(125).    % downtown
+t(_)::joy_place(139).    % fastfood_restaurant
+t(_)::joy_place(153).    % formal_garden
+t(_)::joy_place(164).    % golf_course
+t(_)::joy_place(180).    % hot_spring
+t(_)::joy_place(185).    % ice_cream_parlor
+t(_)::joy_place(197).    % japanese_garden
+t(_)::joy_place(203).    % kitchen
+t(_)::joy_place(209).    % lawn
+t(_)::joy_place(215).    % living_room
+t(_)::joy_place(225).    % martial_arts_gym
+t(_)::joy_place(233).    % mountain_path
+t(_)::joy_place(235).    % movie_theater/indoor
+t(_)::joy_place(243).    % ocean
+t(_)::joy_place(254).    % park
+t(_)::joy_place(259).    % patio
+t(_)::joy_place(265).    % picnic_area
+t(_)::joy_place(267).    % pizzeria
+t(_)::joy_place(268).    % playground
+t(_)::joy_place(269).    % playroom
+t(_)::joy_place(270).    % plaza
+t(_)::joy_place(275).    % racecourse
+t(_)::joy_place(276).    % raceway
+t(_)::joy_place(281).    % recreation_room
+t(_)::joy_place(324).    % swimming_hole
+t(_)::joy_place(325).    % swimming_pool/indoor
+t(_)::joy_place(326).    % swimming_pool/outdoor
+t(_)::joy_place(335).    % toyshop
+t(_)::joy_place(339).    % tree_house
+t(_)::joy_place(351).    % volleyball_court/outdoor
+t(_)::joy_place(353).    % water_park
+t(_)::joy_place(355).    % waterfall
+t(_)::joy_place(364).    % zen_garden
+
+% Places that trigger sadness
+t(_)::sad_place(18).     % army_base
+t(_)::sad_place(43).     % basement
+t(_)::sad_place(69).     % burial_chamber
+t(_)::sad_place(85).     % catacomb
+t(_)::sad_place(86).     % cemetery
+t(_)::sad_place(111).    % crevasse
+t(_)::sad_place(116).    % desert/sand
+t(_)::sad_place(163).    % glacier
+t(_)::sad_place(178).    % hospital
+t(_)::sad_place(179).    % hospital_room
+t(_)::sad_place(186).    % ice_floe
+t(_)::sad_place(187).    % ice_shelf
+t(_)::sad_place(190).    % iceberg
+t(_)::sad_place(191).    % igloo
+t(_)::sad_place(192).    % industrial_area
+t(_)::sad_place(196).    % jail_cell
+t(_)::sad_place(206).    % landfill
+t(_)::sad_place(223).    % market/outdoor (lonely setting)
+t(_)::sad_place(226).    % mausoleum
+t(_)::sad_place(241).    % nursing_home
+t(_)::sad_place(248).    % operating_room
+t(_)::sad_place(292).    % ruin
+t(_)::sad_place(308).    % slum
+t(_)::sad_place(340).    % trench
+t(_)::sad_place(341).    % tundra
+
+% Places that trigger neutrality
+t(_)::neutral_place(1).     % airplane_cabin
+t(_)::neutral_place(2).     % airport_terminal
+t(_)::neutral_place(19).    % art_gallery
+t(_)::neutral_place(25).    % atrium/public
+t(_)::neutral_place(27).    % auditorium
+t(_)::neutral_place(45).    % bathroom
+t(_)::neutral_place(60).    % bookstore
+t(_)::neutral_place(71).    % bus_station/indoor
+t(_)::neutral_place(75).    % cafeteria
+t(_)::neutral_place(92).    % classroom
+t(_)::neutral_place(100).   % computer_room
+t(_)::neutral_place(101).   % conference_center
+t(_)::neutral_place(102).   % conference_room
+t(_)::neutral_place(106).   % corridor
+t(_)::neutral_place(109).   % courtyard
+t(_)::neutral_place(112).   % crosswalk
+t(_)::neutral_place(114).   % delicatessen
+t(_)::neutral_place(115).   % department_store
+t(_)::neutral_place(128).   % drugstore
+t(_)::neutral_place(130).   % elevator_lobby
+t(_)::neutral_place(134).   % entrance_hall
+t(_)::neutral_place(135).   % escalator/indoor
+t(_)::neutral_place(144).   % fire_station
+t(_)::neutral_place(156).   % garage/indoor
+t(_)::neutral_place(158).   % gas_station
+t(_)::neutral_place(160).   % general_store/indoor
+t(_)::neutral_place(172).   % hardware_store
+t(_)::neutral_place(176).   % home_office
+t(_)::neutral_place(182).   % hotel_room
+t(_)::neutral_place(212).   % library/indoor
+t(_)::neutral_place(217).   % lobby
+t(_)::neutral_place(244).   % office
+t(_)::neutral_place(245).   % office_building
+t(_)::neutral_place(246).   % office_cubicles
+t(_)::neutral_place(255).   % parking_garage/indoor
+t(_)::neutral_place(257).   % parking_lot
+t(_)::neutral_place(273).   % promenade
+t(_)::neutral_place(284).   % restaurant
+t(_)::neutral_place(296).   % schoolhouse
+t(_)::neutral_place(302).   % shopping_mall/indoor
+t(_)::neutral_place(317).   % staircase
+t(_)::neutral_place(319).   % street
+t(_)::neutral_place(320).   % subway_station/platform
+t(_)::neutral_place(321).   % supermarket
+t(_)::neutral_place(337).   % train_station/platform
+t(_)::neutral_place(352).   % waiting_room
+t(_)::neutral_place(362).   % yard
+
 
