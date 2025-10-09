cc81fdb92eaeb29e8a19c1d2dec73e334cc0caaf
JarneDemoen <jarne.demoen@vub.be>
2025-10-09 13:07:17 +0200

    Push

--- code/model.pl
+++ code/model.pl
@@ -1,12 +1,11 @@
 
-% Neural networks - flattened tensor approach
+% Neural networks = flattened tensor 
 nn(multi_face_net, [X], Y, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]) :: all_face_emotions(X, Y).
 
 % Scene prediction: 365 scene categories
 nn(scene_net, [X], Y, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285, 286, 287, 288, 289, 290, 291, 292, 293, 294, 295, 296, 297, 298, 299, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328, 329, 330, 331, 332, 333, 334, 335, 336, 337, 338, 339, 340, 341, 342, 343, 344, 345, 346, 347, 348, 349, 350, 351, 352, 353, 354, 355, 356, 357, 358, 359, 360, 361, 362, 363, 364]) :: scene_prediction(X, Y).
 
 % Extract individual face-emotion probability from flattened tensor
-% FlatIndex = FaceIndex * 7 + EmotionIndex
 face_emotion_prob(Image, FaceIdx, EmotionIdx) :-
     between(0, 4, FaceIdx),  
     between(0, 6, EmotionIdx),
@@ -17,13 +16,13 @@ face_emotion_prob(Image, FaceIdx, EmotionIdx) :-
 faces(FaceImg, AllEmotions) :-
     all_face_emotions(FaceImg, AllEmotions).
 
-% Multi-face emotion aggregation (considers ALL faces)
+% Multi-face emotion aggregation (considers ALL faces (I think?))
 multi_face_emotion(Image, AggregatedEmotion) :-
     face_emotion_prob(Image, _, AggregatedEmotion).
 
 % MAIN PREDICATE 
 final_emotion(FaceImg, SceneImg, FindingEmoIdx) :-
-    multi_face_emotion(FaceImg, ModelEmotion),  % Uses all the faces
+    multi_face_emotion(FaceImg, ModelEmotion), 
     scene_valence(SceneImg, SceneValence),
     scene_emotion_boost(ModelEmotion, SceneValence),
     mapped_emotion(ModelEmotion, FindingEmoIdx).
@@ -44,17 +43,16 @@ scene_valence(Image, neutral) :-
     scene(Image, SceneIdx),
     neutral_place(SceneIdx).
 
-% Scene-emotion compatibility rules with probabilistic weights
-t(_)::scene_emotion_boost(3, joy).     % Happy emotion + joy place = strong boost
-t(_)::scene_emotion_boost(4, sad).     % Sad emotion + sad place = strong boost  
-t(_)::scene_emotion_boost(6, neutral). % Neutral emotion + neutral place = medium boost
-t(_)::scene_emotion_boost(3, sad).     % Happy emotion + sad place = weak (contradiction)
-t(_)::scene_emotion_boost(4, joy).     % Sad emotion + joy place = weak (contradiction)
-t(_)::scene_emotion_boost(_, neutral). % Any emotion + neutral place = medium
-t(_)::scene_emotion_boost(_, _).       % Default compatibility
+t(_)::scene_emotion_boost(3, joy). % Happy emotion + joy place
+t(_)::scene_emotion_boost(4, sad). % Sad emotion + sad place   
+t(_)::scene_emotion_boost(6, neutral). % Neutral emotion + neutral place 
+t(_)::scene_emotion_boost(3, sad). % Happy emotion + sad place
+t(_)::scene_emotion_boost(4, joy). % Sad emotion + joy place
+t(_)::scene_emotion_boost(_, neutral). % Any emotion + neutral place
+t(_)::scene_emotion_boost(_, _). % Default compatibility
 
 % Learn mapping of model emotions to dataset emotions
-% Angry -> rage, annoyance
+% Angry -> anger, rage, annoyance
 t(_)::mapped_emotion(0, 4).
 t(_)::mapped_emotion(0, 13).
 t(_)::mapped_emotion(0, 14).
