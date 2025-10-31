481ed4e46f461fcfd0eff78173da0b77dddad2b2
JarneDemoen <jarne.demoen@vub.be>
2025-10-31 14:17:56 +0100

    Need to presave the faces again

--- code/prolog/model3.pl
+++ code/prolog/model3.pl
@@ -1,35 +1,30 @@
-nn(face_model, [Face], Emotion, [angry, disgust, fear, happy, sad, surprise]):: face_emotion(Face, Emotion).
+nn(face_model, [Face], Emotion, [anger, disgust, fear, joy, sadness, surprise]):: face_emotion(Face, Emotion).
 
-nn(scene_model, [Image], Scene, ['natural_landscape',
- 'industrial_facility',
- 'urban_outdoor',
- 'indoor_residential',
- 'religious_or_historical_site',
- 'sports_and_entertainment',
- 'cultural_space',
- 'transport_infrastructure',
- 'aquatic_environment',
- 'cold_environment',
- 'indoor_institutional',
- 'rural_or_recreational_area',
- 'indoor_commercial']):: scene(Image, Scene).
+nn(scene_model, [Image], Scene, [natural_landscape,
+ industrial_facility,
+ urban_outdoor,
+ indoor_residential,
+ religious_or_historical_site,
+ sports_and_entertainment,
+ cultural_space,
+ transport_infrastructure,
+ aquatic_environment,
+ cold_environment,
+ indoor_institutional,
+ rural_or_recreational_area,
+ indoor_commercial]):: scene(Image, Scene).
 
-face_emotion_1([Face_1, _, _], Emotion):-
+t(0.2):: context_to_emotion(natural_landscape, joy).
+
+
+face_emotion_0([Face_0, _, _], Emotion):-
+    face_emotion(Face_0, Emotion).
+
+face_emotion_1([_, Face_1, _], Emotion):-
     face_emotion(Face_1, Emotion).
 
-face_emotion_2([_, Face_2, _], Emotion):-
+face_emotion_2([_, _, Face_2], Emotion):-
     face_emotion(Face_2, Emotion).
-
-face_emotion_3([_, _, Face_3], Emotion):-
-    face_emotion(Face_3, Emotion).
 
 scene_context(Scene, Context):-
     scene(Scene, Context).
-
-final_emo(Faces, Scene, E):-
-    face_emotion_1(Faces, E).
-    % face_emotion_2(Faces, Emo_2),
-    % face_emotion_3(Faces, Emo_3),
-    % scene(Scene, Context),
-    % context_to_emotion(Context, Context_Emo),
-    % aggregate_emotion(Emo_1, Emo_2, Emo_3, Context_Emo, E).
