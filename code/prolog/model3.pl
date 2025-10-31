a67ec874459c814db35346ce922f6bd81fd3b579
JarneDemoen <jarne.demoen@vub.be>
2025-10-31 08:29:09 +0100

    Integrated simple queries again

--- code/prolog/model3.pl
+++ code/prolog/model3.pl
@@ -1,12 +1,24 @@
-nn(face_model, [Face], Emotion, [angry, disgust, fear, happy, sad, surprise]):- face_emotion(Face, Emotion).
+nn(face_model, [Face], Emotion, [angry, disgust, fear, happy, sad, surprise]):: face_emotion(Face, Emotion).
 
 nn(scene_model, [Image], Scene, [aquatic_environment,cold_environment,cultural_space,
                                 indoor_commercial, indoor_institutional, indoor_residential,
                                 industrial_facility, natural_landscape, religious_or_historical_site,
                                 rural_or_recreational_area, sports_and_entertainment, transport_infrastructure,
-                                urban_outdoor]):- scene(Image, Scene)
+                                urban_outdoor]):: scene(Image, Scene).
 
-group_emotion(Face1, Face2, Face3, E):-
-    face_emotion(Face1, E1),
-    face_emotion(Face2, E2),
-    face_emotion(Face3, E3).
+face_emotion_1([Face_1, _, _], Emotion):-
+    face_emotion(Face_1, Emotion).
+
+face_emotion_2([_, Face_2, _], Emotion):-
+    face_emotion(Face_2, Emotion).
+
+face_emotion_3([_, _, Face_3], Emotion):-
+    face_emotion(Face_3, Emotion).
+
+final_emo(Faces, Scene, E):-
+    face_emotion_1(Faces, E).
+    % face_emotion_2(Faces, Emo_2),
+    % face_emotion_3(Faces, Emo_3),
+    % scene(Scene, Context),
+    % context_to_emotion(Context, Context_Emo),
+    % aggregate_emotion(Emo_1, Emo_2, Emo_3, Context_Emo, E).
