2fae89c06adbfff14e9a38a64aa75619ec60274b
JarneDemoen <jarne.demoen@vub.be>
2025-10-31 13:25:52 +0100

    We get nice predictions from pretrained models, mapping to 13 categories works fine via matrix multiplication

--- code/prolog/model3.pl
+++ code/prolog/model3.pl
@@ -1,10 +1,18 @@
 nn(face_model, [Face], Emotion, [angry, disgust, fear, happy, sad, surprise]):: face_emotion(Face, Emotion).
 
-nn(scene_model, [Image], Scene, [aquatic_environment,cold_environment,cultural_space,
-                                indoor_commercial, indoor_institutional, indoor_residential,
-                                industrial_facility, natural_landscape, religious_or_historical_site,
-                                rural_or_recreational_area, sports_and_entertainment, transport_infrastructure,
-                                urban_outdoor]):: scene(Image, Scene).
+nn(scene_model, [Image], Scene, ['natural_landscape',
+ 'industrial_facility',
+ 'urban_outdoor',
+ 'indoor_residential',
+ 'religious_or_historical_site',
+ 'sports_and_entertainment',
+ 'cultural_space',
+ 'transport_infrastructure',
+ 'aquatic_environment',
+ 'cold_environment',
+ 'indoor_institutional',
+ 'rural_or_recreational_area',
+ 'indoor_commercial']):: scene(Image, Scene).
 
 face_emotion_1([Face_1, _, _], Emotion):-
     face_emotion(Face_1, Emotion).
@@ -14,6 +22,9 @@ face_emotion_2([_, Face_2, _], Emotion):-
 
 face_emotion_3([_, _, Face_3], Emotion):-
     face_emotion(Face_3, Emotion).
+
+scene_context(Scene, Context):-
+    scene(Scene, Context).
 
 final_emo(Faces, Scene, E):-
     face_emotion_1(Faces, E).
