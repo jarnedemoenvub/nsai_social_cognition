dd0d8037aa53167e57cb50edf42e8a48537f22f7
JarneDemoen <jarne.demoen@vub.be>
2025-11-24 19:11:24 +0100

    Goin to drop val ar logic as its inherently implemented in the scene clustering. DPL needs to learn mapping of prob based dom face emotion plus scenecluster to final emo

--- code/prolog/model5.pl
+++ code/prolog/model5.pl
@@ -5,46 +5,35 @@ nn(face_model, [Face], Emotion,
 nn(scene_model, [Scene], Cluster, [0, 1, 2, 3, 4, 5, 6, 7, 8]) ::
    scene_cluster(Scene, Cluster).
 
-va_cluster(very_low, very_high, 0).
+va_cluster(low, high, 0).
 va_cluster(mid, mid, 1).
-va_cluster(low, mid, 2).
-va_cluster(very_high, high, 3).
-va_cluster(very_high, low, 4).
-va_cluster(high, very_low, 5).
-va_cluster(mid, very_low, 6).
-va_cluster(low, high, 7).
-va_cluster(very_high, very_low, 8).
+t(_)::va_cluster(low, mid, 2);
+t(_)::va_cluster(low, mid, 7).
 
-va_emotion(high, very_low, acceptance).
-va_emotion(very_high, mid, admiration).
-va_emotion(very_high, mid, amazement).
-va_emotion(very_low, very_high, anger).
-va_emotion(very_low, low, annoyance).
-va_emotion(mid, low, anticipation).
-va_emotion(low, low, apprehension).
-va_emotion(low, very_low, boredom).
-va_emotion(very_low, mid, disgust).
-va_emotion(mid, very_low, distraction).
-va_emotion(very_high, very_high, ecstasy).
-va_emotion(very_low, high, fear).
-va_emotion(very_low, very_high, grief).
-va_emotion(high, very_low, interest).
-va_emotion(very_high, mid, joy).
-va_emotion(very_low, high, loathing).
-va_emotion(low, low, pensiveness).
-va_emotion(very_low, very_high, rage).
-va_emotion(very_low, high, sadness).
-va_emotion(very_high, low, serenity).
-va_emotion(mid, low, surprise).
-va_emotion(very_low, very_high, terror).
-va_emotion(very_high, low, trust).
-va_emotion(mid, mid, vigilance).
+va_cluster(high, high, 3).
+t(_)::va_cluster(high, low, 4);
+t(_)::va_cluster(high, low, 5);
+t(_)::va_cluster(high, low, 8).
 
-va_face_emotion(low, mid, angry).
-va_face_emotion(low, high, disgust).
-va_face_emotion(low, mid, fear).
+va_cluster(mid, low, 6).
+
+t(_)::va_emotion(low, high, anger);
+t(_)::va_emotion(low, high, fear);
+t(_)::va_emotion(low, high, sadness).
+
+t(_)::va_emotion(mid, low, anticipation);
+t(_)::va_emotion(mid, low, surprise).
+
+va_emotion(low, low, disgust).
+va_emotion(high, mid, joy).
+va_emotion(high, low, trust).
+
+t(_)::va_face_emotion(low, mid, angry);
+t(_)::va_face_emotion(low, mid, disgust);
+t(_)::va_face_emotion(low, mid, sad).
+
+t(_)::va_face_emotion(low, low, fear);
+t(_)::va_face_emotion(low, low, neutral).
+
 va_face_emotion(high, mid, happy).
-va_face_emotion(low, low, neutral).
-va_face_emotion(low, mid, sad).
 va_face_emotion(low, high, surprise).
-
--- code/prolog/model5_draft.pl
+++ code/prolog/model5_draft.pl
@@ -5,170 +5,49 @@ nn(face_model, [Face], Emotion,
 nn(scene_model, [Scene], Cluster, [0, 1, 2, 3, 4, 5, 6, 7, 8]) ::
    scene_cluster(Scene, Cluster).
 
-final_emotion_0([], SceneTensor, FinalEmo) :-
-   dont_move_to_neighbour,
-   scene_cluster(SceneTensor, Cluster),
-   va_cluster(Cluster, Valence, Arousal),
-   va_emotion(Valence, Arousal, FinalEmo).
+va_cluster(low, high, 0).
+va_cluster(mid, mid, 1).
+t(_)::va_cluster(low, mid, 2);
+t(_)::va_cluster(low, mid, 7).
 
-final_emotion_0([], SceneTensor, FinalEmo):-
-   scene_cluster(SceneTensor, Cluster),
-   va_cluster(Cluster, Valence, Arousal),
-   % In case move_to_neighbour is true
-   move_to_neighbour,
-   % With 50% chance choose neighbour of Valence, 50% choose neighbour of Arousal (make probs trainable)
-   va_neighbour(Valence, Arousal, FinalEmo).
+va_cluster(high, high, 3).
+t(_)::va_cluster(high, low, 4);
+t(_)::va_cluster(high, low, 5);
+t(_)::va_cluster(high, low, 8).
 
-final_emotion_0([], SceneTensor, FinalEmo):-
-   dont_move_to_neighbour,
-   scene_cluster(SceneTensor, Cluster),
-   va_cluster(Cluster, Valence, Arousal),
-   % In case no emotion was found for the valence/arousal combo, find the nearest neighbour
-   \+ va_emotion(Valence, Arousal, _),
-   % With 50% chance choose neighbour of Valence, 50% choose neighbour of Arousal (make probs trainable)
-   va_neighbour(Valence, Arousal, FinalEmo).
+va_cluster(mid, low, 6).
 
-higher_value(very_low, low).
-higher_value(very_low, mid).
-higher_value(very_low, high).
-higher_value(very_low, very_high).
+t(_)::va_emotion(low, high, anger);
+t(_)::va_emotion(low, high, fear);
+t(_)::va_emotion(low, high, sadness).
 
-higher_value(low, mid).
-higher_value(low, high).
-higher_value(low, very_high).
-
-higher_value(mid, high).
-higher_value(mid, very_high).
-
-higher_value(high, very_high).
-
-lower_value(very_high, high).
-lower_value(very_high, mid).
-lower_value(very_high, low).
-lower_value(very_high, very_low).
-
-lower_value(high, mid).
-lower_value(high, low).
-lower_value(high, very_low).
-
-lower_value(mid, low).
-lower_value(mid, very_low).
-
-lower_value(low, very_low).
-
-t(0.5) :: valence_or_arousal(arousal);
-t(0.5) :: valence_or_arousal(valence).
-
-t(0.5) :: higher_or_lower(higher);
-t(0.5) :: higher_or_lower(lower).
-
-t(0.15) :: move_to_neighbour;
-t(0.85) :: dont_move_to_neighbour.
-
-% Valence
-va_neighbour(very_high, Arousal, FinalEmo):-
-   valence_or_arousal(valence),
-   % If feature is valence, we change the valence
-   % If valence is already the lowest or highest value, we can already change that
-   lower_value(very_high, NewValence),
-   va_emotion(NewValence, Arousal, FinalEmo).
-
-va_neighbour(very_low, Arousal, FinalEmo):-
-   valence_or_arousal(valence),
-   % If feature is valence, we change the valence
-   % If valence is already the lowest or highest value, we can already change that
-   higher_value(very_low, NewValence),
-   va_emotion(NewValence, Arousal, FinalEmo).
-
-va_neighbour(Valence, Arousal, FinalEmo):-
-   valence_or_arousal(valence),
-   % If feature is valence, we change the valence
-   % We change it to a higher value
-   higher_or_lower(higher),
-   higher_value(Valence, NewValence),
-   va_emotion(NewValence, Arousal, FinalEmo).
-
-va_neighbour(Valence, Arousal, FinalEmo):-
-   valence_or_arousal(valence),
-   % If feature is valence, we change the valence
-   % We change it to a lower value
-   higher_or_lower(lower),
-   lower_value(Valence, NewValence),
-   va_emotion(NewValence, Arousal, FinalEmo).
-
-% Arousal
-va_neighbour(Valence, very_high, FinalEmo):-
-   valence_or_arousal(arousal),
-   % If feature is valence, we change the valence
-   % If valence is already the lowest or highest value, we can already change that
-   lower_value(very_high, NewArousal),
-   va_emotion(Valence, NewArousal, FinalEmo).
-
-va_neighbour(Valence, very_low, FinalEmo):-
-   valence_or_arousal(arousal),
-   % If feature is valence, we change the valence
-   % If valence is already the lowest or highest value, we can already change that
-   higher_value(very_low, NewArousal),
-   va_emotion(Valence, NewArousal, FinalEmo).
-
-va_neighbour(Valence, Arousal, FinalEmo):-
-   valence_or_arousal(arousal),
-   % If feature is valence, we change the valence
-   % We change it to a higher value
-   higher_or_lower(higher),
-   higher_value(Arousal, NewArousal),
-   va_emotion(Valence, NewArousal, FinalEmo).
-
-va_neighbour(Valence, Arousal, FinalEmo):-
-   valence_or_arousal(arousal),
-   % If feature is valence, we change the valence
-   % We change it to a lower value
-   higher_or_lower(lower),
-   lower_value(Arousal, NewArousal),
-   va_emotion(Valence, NewArousal, FinalEmo).
-
-% Zorg ervoor dat elke cluster wel naar een emotie mapt en dat alle emoties ook effectief bereikbaar zijn!
-va_cluster(0, very_low, very_high).
-va_cluster(1, mid, mid).
-va_cluster(2, low, mid).
-va_cluster(3, very_high, high).
-va_cluster(4, very_high, low).
-va_cluster(5, high, very_low).
-va_cluster(6, mid, very_low).
-va_cluster(7, low, high).
-va_cluster(8, very_high, very_low).
-
-t(_)::va_emotion(high, very_low, acceptance);
-t(_)::va_emotion(high, very_low, interest).
-
-t(_)::va_emotion(very_high, mid, admiration);
-t(_)::va_emotion(very_high, mid, amazement);
-t(_)::va_emotion(very_high, mid, joy).
-
-t(_)::va_emotion(very_low, high, anger);
-t(_)::va_emotion(very_low, high, fear);
-t(_)::va_emotion(very_low, high, loathing);
-t(_)::va_emotion(very_low, high, sadness).
-
-va_emotion(very_low, low, annoyance).
 t(_)::va_emotion(mid, low, anticipation);
 t(_)::va_emotion(mid, low, surprise).
 
-t(_)::va_emotion(low, low, apprehension);
-t(_)::va_emotion(low, low, pensiveness).
+va_emotion(low, low, disgust).
+va_emotion(high, mid, joy).
+va_emotion(high, low, trust).
 
-va_emotion(low, very_low, boredom).
-va_emotion(very_low, mid, disgust).
-va_emotion(mid, very_low, distraction).
-va_emotion(very_high, high, ecstasy).
+t(_)::va_face_emotion(low, high, angry);
+t(_)::va_face_emotion(low, high, fear).
 
-% Cluster 0
-t(_)::va_emotion(very_low, very_high, grief);
-t(_)::va_emotion(very_low, very_high, rage);
-t(_)::va_emotion(very_low, very_high, terror).
+t(_)::va_face_emotion(low, low, disgust);
+t(_)::va_face_emotion(low, low, sad).
 
-va_emotion(very_high, very_low, serenity).
-va_emotion(very_high, low, trust).
+va_face_emotion(high, mid, happy).
+va_face_emotion(mid, low, neutral).
+va_face_emotion(mid, high, surprise).
 
-% Cluster 1
-va_emotion(mid, mid, vigilance).
+final_emotion_0([], SceneTensor, FinalEmo) :-
+    scene_cluster(SceneTensor, Cluster),
+    va_cluster(Val, Ar, Cluster),
+    va_emotion(Val, Arou, FinalEmo).
+
+final_emotion_1([FaceTensor1], SceneTensor, FinalEmo) :-
+   scene_cluster(SceneTensor, Cluster),
+   face_emotion(FaceTensor1, FaceEmotion),
+   % Based on the face prediction, get symboloc values for valence and arousal
+   va_face_emotion(FaceVal, FaceAr, FaceEmotion),
+   % Now get the estimated symbolic value for valence and arousal based on the scene
+   scene_cluster(SceneTensor, CLuster),
+   va_cluster(SceneVal, SceneAr, Cluster),
