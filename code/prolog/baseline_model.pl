46d68d8ec8893da478985381255bbc62ed93b067
JarneDemoen <jarne.demoen@vub.be>
2025-11-29 19:21:35 +0100

    They train the same and have quite good results. Easy now to test if symbolic knowledge will help!

--- code/prolog/baseline_model.pl
+++ code/prolog/baseline_model.pl
@@ -1,1 +1,4 @@
-nn(baseline_dpl, [X], Y, [0, 1, 2, 3, 4, 5, 6, 7]) :: final_emo(X, Y).
+nn(baseline_dpl, [X], Y, [0, 1, 2, 3, 4, 5, 6, 7]) :: get_baseline_pred(X, Y).
+
+final_emo(Features, FindingEmo):-
+    get_baseline_pred(Features, FindingEmo).
